18fd9fd55184616bfc9979407de76869a15dbbc9
rajkonkret <52714027+rajkonkret@users.noreply.github.com>
2023-09-11 11:54:42 +0200

    add pierwszy

---
+++ pierwszy.py
@@ -0,0 +1,48 @@
+print()  # wyswietlanie np na ekranie
+# PEP8  - zasady czystego kodu
+# ctrl alt l
+print("Nazywam się Radek")  # Nazywam się Radek
+print('Nazywam się Radek')  # Nazywam się Radek
+# ctrl d - kopiowanie liniilinii
+# ctrl d - kopiowanie linii
+# ctrl d - kopiowanie linii
+# znaczek komentarza, python nie wczyta rzeczy z komentarza
+# type()  # wypisanie typu
+print(type("Radek"))  # <class 'str'>
+print("Radek")  # Radek
+# str - czyli typ tekstowy
+# 11:30
+
+print("39")
+print(type("39"))  # <class 'str'>
+print("39" + "14")  # 3914 - łączenie tekstów
+# konkatenacja - łaczenie tekstów
+print(5 * "4")  # 44444
+
+# typ liczby całkowite - int
+print(39)
+print(type(39))  # <class 'int'>
+
+print(39 + 14)  # 53
+# print("39" + 14)  # TypeError: can only concatenate str (not "int") to str
+print("39" + str(14))  # 3914
+# str() - rzutowanie na typ str (tekstowy)
+
+# zmienna - pudełko na dane
+wiek  = 48
+print(wiek)
+print(type(wiek))  # <class 'int'>
+
+imie = "Radek"
+print(imie)
+print(type(imie))  # <class 'str'>
+
+wiek = "Radek"
+print(wiek)
+print(type(wiek))  # <class 'str'>
+
+wiek : int = 56  # podpowiedź o typie
+print(wiek)
+wiek = "tekst"
+print(wiek)
+
